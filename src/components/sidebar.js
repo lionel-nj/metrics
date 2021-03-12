97cefe0b36d84826c5c80410a331daab73719d70
lionel-nj <lionel@mobilitydata.org>
2021-03-12 13:37:56 -0500

    add emojis

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,23 +3,23 @@ import { slide as Menu } from "react-burger-menu";
 
 export default props => {
   return (
-    // Pass on our props
-    <Menu {...props}>
-      <a className="menu-item" href="/">
-        Home
-      </a>
+      // Pass on our props
+      <Menu {...props}>
+        <a className="menu-item" href="/">
+          🏡 Home
+        </a>
 
-      <a className="menu-item" href="/validator">
-        MobilityData/gtfs-validator
-      </a>
+        <a className="menu-item" href="/validator">
+          🔧 MobilityData/gtfs-validator
+        </a>
 
-      <a className="menu-item" href="/gtfs">
-        google/transit
-      </a>
+        <a className="menu-item" href="/gtfs">
+          🚂 google/transit
+        </a>
 
-      <a className="menu-item" href="/gbfs">
-        NABSA/gbfs
-      </a>
-    </Menu>
+        <a className="menu-item" href="/gbfs">
+          🚲 NABSA/gbfs
+        </a>
+      </Menu>
   );
 };
