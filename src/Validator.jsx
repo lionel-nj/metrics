a2bc77639ab37b928945311777655690fe72ba19
lionel-nj <lionel@mobilitydata.org>
2021-03-12 15:00:49 -0500

    data plots placeholders

--- src/Validator.jsx
+++ src/Validator.jsx
@@ -1,16 +1,63 @@
 import React from "react";
+import * as Plotly from "plotly.js";
+import Plot from 'react-plotly.js';
 
 function Validator() {
-  // open data on disk + display data
+  // let openedIssueData =
+  // let mergedPrData =
+  // let issueCommentData =
+  // let prCommentData =
+  let xList = [2, 6, 3]
+  let yList = [2, 6, 3]
   return (
-      <div className="home">
+      <div className="validator">
         <h1 className="font-weight-light"><a
             href="https://github.com/MobilityData/gtfs-validator">MobilityData/gtfs-validator</a></h1>
-        <div className="top-left"></div>
-        <div className="top-right"></div>
-        <div className="middle"></div>
-        <div className="bottom-left"></div>
-        <div className="bottom-right"></div>
+        <div className="grid-container">
+          <div className="top-left" id="validator-open-issues">
+            <Plot
+                data={[
+                  {
+                    x: xList,
+                    y: yList,
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    marker: {color: 'red'},
+                  },
+                ]}
+                layout={ {title: 'Opened issue count'} }
+            />
+          </div>
+          <div className="top-right" id='validator-merged-prs'>
+            <Plot
+                data={[
+                  {
+                    x: xList,
+                    y: yList,
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    marker: {color: 'red'},
+                  },
+                ]}
+                layout={ {title: 'Merged PRs count'} }
+            />
+          </div>
+          <div className="middle-left" id='validator-top-issue'></div>
+          <div className="middle-right" id='validator-top-pr'><p id='x'>33 ⭐</p>️</div>
+          <div className="bottom" id='validator-comments'>
+            <Plot
+              data={[
+                {
+                  x: xList,
+                  y: yList,
+                  type: 'scatter',
+                  mode: 'lines+markers',
+                  marker: {color: 'red'},
+                },
+              ]}
+              layout={ {title: 'Comment count'} }
+          /></div>
+        </div>
       </div>
   );
 }
